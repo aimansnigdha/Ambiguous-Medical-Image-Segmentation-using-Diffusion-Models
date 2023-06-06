a9677afe4eedb163db478cd01ea8228161448b33
aimansnigdha <irsnigdha@gmail.com>
2023-06-06 15:05:49 -0400

    Update segmentation_train.py

--- scripts/segmentation_train.py
+++ scripts/segmentation_train.py
@@ -2,38 +2,13 @@
 Train a diffusion model on images.
 """
 import sys
-import argparse
-sys.path.append("..")
-sys.path.append(".")
-from guided_diffusion import dist_util, logger
-from guided_diffusion.resample import create_named_schedule_sampler
-from torch.utils.data.sampler import SubsetRandomSampler
-from guided_diffusion.bratsloader import BRATSDataset
-from guided_diffusion.lidcloader import LIDCDataset
-from guided_diffusion.script_util import (
-    model_and_diffusion_defaults,
-    create_model_and_diffusion,
-    args_to_dict,
-    add_dict_to_argparser,
-)
-import torch as th
-from guided_diffusion.train_util import TrainLoop
-from visdom import Visdom
-viz = Visdom(port=8097)
-
-def main():"""
-Train a diffusion model on images.
-"""
-import sys
 import os
-import torch
 import argparse
 sys.path.append("..")
 sys.path.append(".")
 from guided_diffusion import dist_util, logger
 from guided_diffusion.resample import create_named_schedule_sampler
 from torch.utils.data.sampler import SubsetRandomSampler
-from guided_diffusion.bratsloader import BRATSDataset
 from guided_diffusion.lidcloader import LIDCDataset
 from guided_diffusion.script_util import (
     model_and_diffusion_defaults,
@@ -42,32 +17,29 @@ from guided_diffusion.script_util import (
     add_dict_to_argparser,
 )
 import torch as th
+import torch
 from guided_diffusion.train_util import TrainLoop
 from visdom import Visdom
 viz = Visdom(port=8097)
 
 def main():
     args = create_argparser().parse_args()
-    
     world_size = args.ngpu
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = "3,5"
-    os.environ["MASTER_PORT"] = str(1028)
+    os.environ['CUDA_VISIBLE_DEVICES'] = "6,7,8"
     
     torch.distributed.init_process_group(
     'gloo',
     init_method='env://',
     world_size=world_size,
     rank=args.local_rank,)
-    
-    
+
+    #dist_util.setup_dist()
     logger.configure()
 
     logger.log("creating model, diffusion, prior and posterior distribution...")
     model, diffusion, prior, posterior = create_model_and_diffusion(
         **args_to_dict(args, model_and_diffusion_defaults().keys())
     )
-    
     
     torch.cuda.set_device(args.local_rank)
     
@@ -79,98 +51,36 @@ def main():
     output_device=args.local_rank,
 )
     
-    
     prior.to(dist_util.dev())
+    prior = torch.nn.SyncBatchNorm.convert_sync_batchnorm(prior)
+    prior = torch.nn.parallel.DistributedDataParallel(
+    prior,
+    device_ids=[args.local_rank],
+    output_device=args.local_rank,
+)
         
     posterior.to(dist_util.dev())
-
+    
+    posterior.to(dist_util.dev())
+    posterior = torch.nn.SyncBatchNorm.convert_sync_batchnorm(posterior)
+    posterior = torch.nn.parallel.DistributedDataParallel(
+    posterior,
+    device_ids=[args.local_rank],
+    output_device=args.local_rank,
+)
+    
+    
     schedule_sampler = create_named_schedule_sampler(args.schedule_sampler, diffusion,  maxt=1000)
 
     logger.log("creating data loader...")
     ds = LIDCDataset(args.data_dir, test_flag=False)
-    
+
+        
     sampler = torch.utils.data.distributed.DistributedSampler(
     ds,
     num_replicas=args.ngpu,
     rank=args.local_rank,
 )
-    
-    
-    datal= th.utils.data.DataLoader(
-        ds,
-        batch_size=args.batch_size,
-        sampler = sampler)
-    data = iter(datal)
-    
-
-
-    logger.log("training...")
-    TrainLoop(
-        model=model,
-        diffusion=diffusion,
-        classifier=None,
-        prior =prior,
-        posterior = posterior,
-        data=data,
-        dataloader=datal,
-        batch_size=args.batch_size,
-        microbatch=args.microbatch,
-        lr=args.lr,
-        ema_rate=args.ema_rate,
-        log_interval=args.log_interval,
-        save_interval=args.save_interval,
-        resume_checkpoint=args.resume_checkpoint,
-        use_fp16=args.use_fp16,
-        fp16_scale_growth=args.fp16_scale_growth,
-        schedule_sampler=schedule_sampler,
-        weight_decay=args.weight_decay,
-        lr_anneal_steps=args.lr_anneal_steps,
-    ).run_loop()
-
-
-def create_argparser():
-    defaults = dict(
-        data_dir="./data/training",
-        schedule_sampler="uniform",
-        lr=1e-4,
-        weight_decay=0.0,
-        lr_anneal_steps=0,
-        batch_size=1,
-        microbatch=-1,  # -1 disables microbatches
-        ema_rate="0.9999",  # comma-separated list of EMA values
-        log_interval=100,
-        save_interval=5000,
-        resume_checkpoint='',#'"./results/pretrainedmodel.pt",
-        use_fp16=False,
-        fp16_scale_growth=1e-3,
-    )
-    defaults.update(model_and_diffusion_defaults())
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--local_rank', type=int, default=2)
-    parser.add_argument('--ngpu', type=int, default=2)
-    add_dict_to_argparser(parser, defaults)
-    return parser
-
-
-if __name__ == "__main__":
-    main()
-
-    args = create_argparser().parse_args()
-
-    dist_util.setup_dist()
-    logger.configure()
-
-    logger.log("creating model, diffusion, prior and posterior distribution...")
-    model, diffusion, prior, posterior = create_model_and_diffusion(
-        **args_to_dict(args, model_and_diffusion_defaults().keys())
-    )
-    model.to(dist_util.dev())
-    schedule_sampler = create_named_schedule_sampler(args.schedule_sampler, diffusion,  maxt=1000)
-
-    logger.log("creating data loader...")
-    ds = LIDCDataset(args.data_dir, test_flag=False)
-
-    
     
     datal= th.utils.data.DataLoader(
         ds,
@@ -221,6 +131,9 @@ def create_argparser():
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()
+    parser.add_argument('--local_rank', type=int, default=4)
+    parser.add_argument('--ngpu', type=int, default=3)
+   
     add_dict_to_argparser(parser, defaults)
     return parser
 
